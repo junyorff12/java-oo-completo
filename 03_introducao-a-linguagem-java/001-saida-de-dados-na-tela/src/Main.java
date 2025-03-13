14bb4fa5c11d107f3fd892eeab2515eb4e1b36da
junyorff12 <junyorff12@gmail.com>
2025-03-13 10:20:20 -0300

    introduc

---
+++ 03_introducao-a-linguagem-java/001-saida-de-dados-na-tela/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Ao escrever syso ou sysout, mais control + space, a IDE printa na tela o System.out.println();
+		System.out.println("Olá mundo!");
+		
+		// 'Ctrl + F11' compila e executa o programa de forma rápida.
+	}
+}
