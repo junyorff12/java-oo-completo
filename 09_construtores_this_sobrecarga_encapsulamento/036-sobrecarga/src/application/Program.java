818228407600267afc42d8003df1e26eaffd07ee
junyorff12 <junyorff12@gmail.com>
2025-03-13 10:55:32 -0300

    construtor, this e sobrecarga

---
+++ 09_construtores_this_sobrecarga_encapsulamento/036-sobrecarga/src/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		// Construtor padrão.
+		Product p = new Product();
+
+		System.out.println("Enter product data:");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Price: ");
+		double price = sc.nextDouble();
+
+		Product product = new Product(name, price);
+
+		System.out.println("\nProduct data: " + product + "\n");
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		System.out.println("\nUpdated data: " + product + "\n");
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		System.out.println("\nUpdated data: " + product);
+
+		sc.close();
+	}
+}
---
+++ 09_construtores_this_sobrecarga_encapsulamento/036-sobrecarga/src/entities/Product.java
@@ -0,0 +1,47 @@
+package entities;
+
+public class Product {
+
+	public String name;
+	public double price;
+	public int quantity;
+	
+	public Product() {
+		
+	}
+
+	public Product(String name, double price, int quantity) {
+
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	public Product(String name, double price) {
+
+		this.name = name;
+		this.price = price;
+		//this.quantity = 0; // Comando opcional.
+		//quantity = 0; // A palavra 'this' também é opcional.
+	}
+
+	public double totalValueInStock() {
+
+		return price * quantity;
+	}
+
+	public void addProducts(int quantity) {
+
+		this.quantity += quantity;
+	}
+
+	public void removeProducts(int quantity) {
+
+		this.quantity -= quantity;
+	}
+
+	public String toString() {
+
+		return String.format("%s, $ %.2f, %d units, Total: $ %.2f", name, price, quantity, totalValueInStock());
+	}
+}
