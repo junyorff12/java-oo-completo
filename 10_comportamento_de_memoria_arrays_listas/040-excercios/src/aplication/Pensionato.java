98773575b17f36e51a8877fde606e81a37c91e27
junyorff12 <junyorff12@gmail.com>
2025-04-06 21:52:22 -0300

    desafio array: adicionando a instancia do Scanner com Locaale.US.

--- 10_comportamento_de_memoria_arrays_listas/040-excercios/src/aplication/Pensionato.java
+++ 10_comportamento_de_memoria_arrays_listas/040-excercios/src/aplication/Pensionato.java
@@ -1,7 +1,13 @@
 package aplication;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Pensionato {
     public static void main(String[] args) {
-        
+        Locale.setDefault(Locale.US);
+        Scanner sc =  new Scanner(System.in);
+
+        sc.close();
     }
 }
