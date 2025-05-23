c1dd9857f305f43df9d5af2107d8e907db649e52
junyorff12 <junyorff12@gmail.com>
2025-05-23 20:29:05 -0300

    boxing, unboxing e wrapper classes

---
+++ 10_comportamento_de_memoria_arrays_listas/041-boxing-unboxing-wrapper-classes/src/application/Main.java
@@ -0,0 +1,22 @@
+package application;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int x = 20; // valor
+
+		Object obj = x; // boxing (referência)
+
+		int y = (int) obj; unboxing 
+		
+		
+		//Wrapper Classes
+		
+		Integer x = 20;
+		
+		int y = x * 2; //sem necessitade de cast.
+		
+
+	}
+
+}
