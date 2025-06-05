e2db6377aea19a986346fbe48a1548493534b4e5
junyorff12 <junyorff12@gmail.com>
2025-06-05 12:37:24 -0300

    resolvendo exercicio: criando entidade Emploiee

---
+++ 10_comportamento_de_memoria_arrays_listas/044-exercicios/src/entities/Emploee.java
@@ -0,0 +1,42 @@
+package entities;
+
+import java.math.BigDecimal;
+
+public class Emploee {
+	private Integer id;
+	private String nome;
+	private BigDecimal salary;
+	
+	public Emploee(Integer id, String nome, BigDecimal salary) {
+		this.id = id;
+		this.nome = nome;
+		this.salary = salary;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public BigDecimal getSalary() {
+		return salary;
+	}
+	public void setSalary(BigDecimal salary) {
+		this.salary = salary;
+	}
+	
+	public void aumentarSalario(double percentual) {
+		this.salary = this.salary.add(
+				(this.salary.multiply(
+						BigDecimal.valueOf(percentual).divide(BigDecimal.valueOf(100))
+						)));
+	}
+	
+}
