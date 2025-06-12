197ccf9637ade4ba81640c40e1ec978557f10063
junyorff12 <junyorff12@gmail.com>
2025-06-11 21:05:11 -0300

    resolvendo exercicio: finalizado desafio v2

--- 10_comportamento_de_memoria_arrays_listas/044-exercicios/src/application/Main.java
+++ 10_comportamento_de_memoria_arrays_listas/044-exercicios/src/application/Main.java
@@ -11,59 +11,61 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		List<Employee> funcionarios = new ArrayList<Employee>();
-		
+
 		System.out.println("Quantos funcionarios serão registrados? ");
 		int numFuncionarios = sc.nextInt();
-		
+
 		for (int i = 0; i < numFuncionarios; i++) {
 			int employee = i + 1;
 			System.out.println("Employee #" + employee);
 			System.out.print("Id:");
 			int id = sc.nextInt();
-			
+
 			if (idCheck(funcionarios, id) != null) {
 				System.out.println("Id ja registrado, informe outro ID:");
 				id = sc.nextInt();
 			}
-			
+
 			System.out.print("Nome:");
-			sc.nextLine();//limpando o enter
+			sc.nextLine();// limpando o enter
 			String nome = sc.nextLine();
 			System.out.print("Salario:");
 			BigDecimal salario = sc.nextBigDecimal();
-			
+
 			funcionarios.add(i, new Employee(id, nome, salario));
-			
-			System.out.print("Entre com o funcionario que terá salario aumentado:");
-			
-			int funcionarioId = sc.nextInt();
-			
-			System.out.println("Entre com a porcentegem:");
-			
-			double porcentagem = sc.nextDouble();
-			
-			aumentoDeSalario(funcionarios, funcionarioId, porcentagem);
-			
 		}
-		
+
+		System.out.print("Entre com o funcionario que terá salario aumentado:");
+
+		int funcionarioId = sc.nextInt();
+
+		if (idCheck(funcionarios, funcionarioId) == null) {
+			System.out.println("Digite um id válido conforme a listagem abaixo:");
+			funcionarios.stream().forEach(x -> System.out.println(x.getId()));
+			funcionarioId = sc.nextInt();
+		}
+
+		System.out.println("Entre com a porcentegem:");
+
+		double porcentagem = sc.nextDouble();
+
+		aumentoDeSalario(funcionarios, funcionarioId, porcentagem);
+
 		System.out.println("Lista de funcionarios:");
-		funcionarios.stream()
-			.forEach(x -> System.out.println(
-						x.getId() + "," + x.getNome() + "," + x.getSalary()));
-		
+		funcionarios.stream().forEach(x -> System.out.println(x.getId() + "," + x.getNome() + "," + x.getSalary()));
+
 		sc.close();
 
 	}
-	
+
 	public static Employee idCheck(List<Employee> funcionarios, int id) {
-		return funcionarios.stream().filter(x -> x.getId() == id).findAny().orElse(null);	
+		return funcionarios.stream().filter(x -> x.getId() == id).findAny().orElse(null);
 	}
-	
+
 	public static void aumentoDeSalario(List<Employee> funcionarios, int id, double porcentagem) {
-		funcionarios.stream().filter(x -> x.getId() == id)
-		.forEach(x -> x.aumentarSalario(porcentagem));
+		funcionarios.stream().filter(x -> x.getId() == id).forEach(x -> x.aumentarSalario(porcentagem));
 	}
 
 }
