d8a7480bd22cbb6a18c7add90f8256e011daff54
junyorff12 <junyorff12@gmail.com>
2025-06-23 12:13:05 -0300

    exemplo de matriz int

--- 10_comportamento_de_memoria_arrays_listas/045-matriz/src/application/Main.java
+++ 10_comportamento_de_memoria_arrays_listas/045-matriz/src/application/Main.java
@@ -3,7 +3,9 @@ package application;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Integer[][] matriz = {{1,2},{3,4}};
+		
+		System.out.println(matriz[0][0]);
 
 	}
 
