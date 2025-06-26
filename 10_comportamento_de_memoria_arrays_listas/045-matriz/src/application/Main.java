6536311a544950d3ef232ed4ab98899ceba9331a
junyorff12 <junyorff12@gmail.com>
2025-06-25 23:02:56 -0300

    exercicio com matriz

--- 10_comportamento_de_memoria_arrays_listas/045-matriz/src/application/Main.java
+++ 10_comportamento_de_memoria_arrays_listas/045-matriz/src/application/Main.java
@@ -1,12 +1,38 @@
 package application;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Integer[][] matriz = {{1,2},{3,4}};
 		
-		System.out.println(matriz[0][0]);
-
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Digite o numero de linhas e de colunas da matriz:");
+			
+			int n = sc.nextInt();
+			
+			int[][] matriz = new int[n][n];
+			
+			for(int i = 0; i < n; i++) {
+				System.out.println("Digite os numeros da linha " + (i + 1));
+				for (int j = 0; j < n; j++) {
+					System.out.println("Digite os numeros da coluna " + (i + 1));
+					matriz[i][j] = sc.nextInt();
+				}
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			sc.close();
 	}
 
 }
