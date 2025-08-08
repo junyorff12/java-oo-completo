04991bac4e1959176d247ea8199b71dc0a7b0c13
junyorff12 <junyorff12@gmail.com>
2025-08-08 06:53:33 -0300

    exercicio com matriz: Iniciando classe principal.

---
+++ 10_comportamento_de_memoria_arrays_listas/045-matriz/src/exercicios/ValoresAdjacentes.java
@@ -0,0 +1,22 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class ValoresAdjacentes {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite o numero de linhas e de colunas da matriz:");
+		
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		
+		int[][] matriz = new int[m][n];
+		
+		
+		
+		sc.close();
+
+	}
+
+}
