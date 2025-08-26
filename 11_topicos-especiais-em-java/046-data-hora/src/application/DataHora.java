b6c07abf4f6b0f9ecc575878178cc127d9b11194
junyorff12 <junyorff12@gmail.com>
2025-08-26 19:56:43 -0300

    Trabalhando com Instant

--- 11_topicos-especiais-em-java/046-data-hora/src/application/DataHora.java
+++ 11_topicos-especiais-em-java/046-data-hora/src/application/DataHora.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -9,9 +10,11 @@ public class DataHora {
 
 		LocalDate d01 = LocalDate.now();
 		LocalDateTime d02 = LocalDateTime.now();
+		Instant d03 = Instant.now();
 		
 		System.out.println("d01 = " + d01);
-		System.out.println("d02	 = " + d02);
+		System.out.println("d02	= " + d02);
+		System.out.println("d03	= " + d03);
 	}
 
 }
