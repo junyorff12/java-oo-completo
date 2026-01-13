4ecace7a18751cfc1917a56ab14996ef0cfae4ae
junyorff12 <junyorff12@gmail.com>
2026-01-13 18:27:19 -0300

    Praticando com LocalDate, LocalDateTime, Instant, LocalDateFormatter

--- 11_topicos-especiais-em-java/046-data-hora/src/application/DataHora.java
+++ 11_topicos-especiais-em-java/046-data-hora/src/application/DataHora.java
@@ -9,27 +9,36 @@ public class DataHora {
 
 	public static void main(String[] args) {
 		
-		DateTimeFormatter fmt01 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		DateTimeFormatter fmt02 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-		DateTimeFormatter fmt03 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 		
-		LocalDate d05 = LocalDate.parse("25/04/1987", fmt01);
-		LocalDateTime d06 = LocalDateTime.parse("25/04/1987 01:30", fmt02);
-		LocalDateTime d07 = LocalDateTime.parse("25/04/1987 01:30:31", fmt03);
-
 		LocalDate d01 = LocalDate.now();
 		LocalDateTime d02 = LocalDateTime.now();
 		Instant d03 = Instant.now();
 		
-		LocalDate d04 = LocalDate.parse("2025-01-25");
+		LocalDate d04 = LocalDate.parse("2022-07-20");
+		LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+		Instant d06 = Instant.parse("2022-07-20T01:30:30Z");
+		Instant d07 = Instant.parse("2022-07-20T01:30:30-03:00");
+		
+		LocalDate d08 = LocalDate.parse("20/07/2022", fmt1);
+		LocalDateTime d09 = LocalDateTime.parse("20/07/2022 01:30", fmt2);
+		
+		LocalDate d10 = LocalDate.of(2026, 1, 13);
+		LocalDateTime d11 = LocalDateTime.of(2026, 1, 13, 01, 30);
 		
 		System.out.println("d01 = " + d01);
-		System.out.println("d02	= " + d02);
-		System.out.println("d03	= " + d03);
-		System.out.println("d04	= " + d04);
-		System.out.println("d05	= " + d05);
-		System.out.println("d06	= " + d06);
-		System.out.println("d06	= " + d07);
+		System.out.println("d02 = " + d02);
+		System.out.println("d03 = " + d03);
+		System.out.println("d04 = " + d04);
+		System.out.println("d05 = " + d05);
+		System.out.println("d06 = " + d06);
+		System.out.println("d07 = " + d07);
+		System.out.println("d08 = " + d08);
+		System.out.println("d09 = " + d09);
+		System.out.println("d10 = " + d10);
+		System.out.println("d11 = " + d11);
+		
 	}
-
+	
 }
---
+++ 11_topicos-especiais-em-java/046-data-hora/src/application/TesteIgualdadeString.java
@@ -0,0 +1,15 @@
+package application;
+
+public class TesteIgualdadeString {
+
+	public static void main(String[] args) {
+		String nome = "junyor";
+		String nome2 = "junyor";
+		
+		boolean res = nome == nome2;
+		
+		System.out.println(res);
+
+	}
+
+}
