b8349f15530f6887a5a293b55c57d21a7e1a85e4
junyorff12 <junyorff12@gmail.com>
2026-02-08 21:21:23 -0300

    praticando com a classe Period

---
+++ 11_topicos-especiais-em-java/046-data-hora/src/application/Exercicios.java
@@ -0,0 +1,17 @@
+package application;
+
+import java.time.*;
+
+public class Exercicios {
+	
+	public static void main(String[] args) {
+		LocalDate inicialD = LocalDate.parse("2026-01-10");
+		LocalDate finalD = LocalDate.parse("2026-12-20");
+		
+		Period p = Period.between(inicialD, finalD);
+		
+		System.out.println(p.getMonths());
+		System.out.println(p.getYears());
+		System.out.println(p.getDays());
+	}
+}
