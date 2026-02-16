bfa0656fbadac3d41be635d4f2e288e6faa395c1
junyorff12 <junyorff12@gmail.com>
2026-02-15 23:30:51 -0300

    Estudando ZonedDateTime com ZoneId

--- 11_topicos-especiais-em-java/046-data-hora/src/application/ZonedDateTimeEX.java
+++ 11_topicos-especiais-em-java/046-data-hora/src/application/ZonedDateTimeEX.java
@@ -1,9 +1,21 @@
 package application;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 public class ZonedDateTimeEX {
 
 	public static void main(String[] args) {
+		ZonedDateTime agora = ZonedDateTime.now();
 		
+		System.out.println(agora);
+		
+		ZonedDateTime data = ZonedDateTime.of(
+		        2026, 2, 14,
+		        10, 30, 0, 0,
+		        ZoneId.of("Europe/Paris")
+		);
+		System.out.println(data);
 		
 	}
 
