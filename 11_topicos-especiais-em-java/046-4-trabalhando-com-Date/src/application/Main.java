a3d9822eb866bd9f0fcd58d46002a41d013dd505
junyorff12 <junyorff12@gmail.com>
2026-03-01 21:53:18 -0300

    Usando a API legada - Date

---
+++ 11_topicos-especiais-em-java/046-4-trabalhando-com-Date/src/application/Main.java
@@ -0,0 +1,13 @@
+package application;
+
+import java.util.Date;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Date agora = new Date();
+		System.out.println(agora);
+		
+	}
+
+}
