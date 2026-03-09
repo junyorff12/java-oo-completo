a5368bf7154d4919cc35dc425c2a6a36f5530424
junyorff12 <junyorff12@gmail.com>
2026-03-08 21:45:53 -0300

    Criando ENUM Java

--- 12_enumeracoes_composicao/47-enumeracoes/src/application/Program.java
+++ 12_enumeracoes_composicao/47-enumeracoes/src/application/Program.java
@@ -1,9 +1,21 @@
 package application;
 
+public enum DiaDaSemana {
+	SEGUNDA,
+	TERCA,
+	QUARTA,
+	QUINTA,
+	SEXTA,
+	SABADO,
+	DOMINGO
+}
+
+
 public class Program {
 
 	public static void main(String[] args) {
 		//enums
+		
 
 	}
 
